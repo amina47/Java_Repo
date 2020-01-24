24d062313bdb0bf271d81a91e6ee10b38a883403
Amina Basheer <aminambasheer@yahoo.com>
2020-01-23 21:15:24 -0500

    midterm

---
+++ src/basic/midterm.java
@@ -0,0 +1,107 @@
+package basic;
+
+import java.util.Scanner;
+
+public class midterm {
+
+    public midterm(Object scanner) {
+        Object Scanner1 = scanner;
+
+    }
+
+    public double findMissingInt(String[] arrayforMissingInt) {
+        int i = 0;
+        return i;
+
+    }
+
+
+    {
+
+
+        double[] arrayforMissingInt;
+
+        double smallest = arrayforMissingInt[0];
+        int[] numArray = {1, 3, 5, 4, 2, 7};
+        int n = numArray.length;
+        Scanner Scanner = new Scanner ( System.in );
+        {
+
+        }
+
+
+        for (int i = 0; i < numArray.length - 1; i++)
+
+            if (arrayforMissingInt[i] < smallest) {
+                smallest = arrayforMissingInt[i];
+                {
+                    System.out.println ( "ThesmallestMissingint" + smallest );
+                    double smallest1 = smallest;
+
+                }
+                {
+                }
+
+            }
+
+    }
+
+    public static  double elementremoval(double[] arrayForRemoval) {
+
+            System.out.println ( "enter Arrayof numbers" );
+
+            int[] numberarray = {32, 14, 24, 98, 24, 56, 148, 24, 78, 24};
+            int[] newExtractedArray = int RemoveSpecificInt (24, );
+
+            {
+
+                System.out.println ( "extracted number+newExtractedArray" );
+                return  int newExtractedArray;
+            }
+
+
+        }
+    }
+
+
+
+public static  void trafficTicket (String... trafficTicketSystem){
+
+    private class speed<speedlimit> {
+
+
+        int Speedlimit = 70;
+
+
+        private boolean isSpeed>speedlimit;
+        private speed(boolean isSpeed) {
+            this.isSpeed = isSpeed;
+            {
+                Object i;
+                speedlimit ( i ) = 70;
+                if (speed >= i) ;
+
+                for (int i = 70 + 5; i + 5) ; {
+                System.out.println ( "add one point" );
+
+                int totalPoint = 0;
+
+                if (totalPoint >= 12) ;
+                {
+                    System.out.println ( "License Suspended" );
+                }
+
+            }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/basic/speedImpl.java
@@ -0,0 +1,4 @@
+package basic;
+
+public class speedImpl extends speed {
+}
